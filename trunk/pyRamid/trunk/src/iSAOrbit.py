5a7ec9eedb59be95df999c29e6b11cb1b56b7b19
hinkocevar@gmail.com <hinkocevar@gmail.com@6af3df78-289f-14e2-590d-b118cb800c92>
2011-06-18 16:58:20 +0000

    pyRamid .. more usable code - verify Qt signal/slot handling..

---
+++ trunk/pyRamid/trunk/src/iSAOrbit.py
@@ -0,0 +1,81 @@
+'''
+Created on Jun 18, 2011
+
+@author: hinko
+'''
+
+#===============================================================================
+# COMMENTS
+#===============================================================================
+#
+#===============================================================================
+
+from iGLobals import iRaise, iLog, iGlobalPVs, iGlobalIOCs, iGlobalHandle
+
+from PyQt4 import QtCore, QtGui
+
+from ui.uiPanelSAOrbit import Ui_PanelSAOrbit
+from lib.iWidgets import iQGraphicsWidget
+
+class iPanelSAOrbit(QtGui.QWidget):
+
+    def __init__(self, parent = None):
+        QtGui.QWidget.__init__(self, parent)
+        iLog.debug("enter")
+
+        self.iocName = None
+        self.pvNames = [':SA:SA_X', ':SA:SA_Y', ':SA:SA_SUM']
+        self.pvs = iGlobalPVs()
+        self.iocs = iGlobalIOCs()
+        self.items = dict()
+
+        self.ui = Ui_PanelSAOrbit()
+        self.ui.setupUi(self)
+
+        self.ui.pushButton_refresh.clicked.connect(self.pvRefresh)
+
+
+    def show(self):
+        iLog.debug("enter")
+
+    def hide(self):
+        iLog.debug("enter")
+
+    def pvPopulate(self):
+        iLog.debug("enter")
+
+        if len(self.items) != 0:
+            iLog.warning("items list already present")
+            return
+
+        pvName = self.pvNames[0]
+        idx = 0
+        for iocName in self.iocs['__myid__']:
+            ioc = self.iocs[iocName]
+            pv = ioc.pv(pvName)
+            item = iQGraphicsWidget(self.ui.graphicsView, idx)
+
+            self.items[pv.nameGetFull()] = item
+            iLog.info("added PV '%s' to view, index %d" % (pv.nameGetFull(), idx))
+            idx += 1
+
+    def pvRefresh(self):
+        iLog.info("enter")
+
+        self.pvPopulate()
+
+        if len(self.items) == 0:
+            iLog.error("invalid items list argument")
+            return
+
+        pvName = self.pvNames[0]
+        for iocName in self.iocs['__myid__']:
+            ioc = self.iocs[iocName]
+            pv = ioc.pv(pvName)
+
+            item = self.items[pv.nameGetFull()]
+
+            item.connectPVObj(pv)
+
+            iLog.debug("Handing over PV '%s' to handler" % pv.nameGetFull())
+            pv.scheduleGet()
